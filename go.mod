e33f1bb4f26794680c5edc0955cf6f8935ad6cf8
Adam Shumann <ashumann13@gmail.com>
2019-04-12 15:09:34 -0500

    Tweaks and improvements.
    Cleaner logging
    Improved error handling
    Updates org names to lowercase
    Added InfluxDB datasource

---
+++ go.mod
@@ -0,0 +1,27 @@
+module github.com/ScreepsPlus/grafana-datasource-manager
+
+go 1.12
+
+require (
+	github.com/Unknwon/com v0.0.0-20190321035513-0fed4efef755 // indirect
+	github.com/bradfitz/gomemcache v0.0.0-20190329173943-551aad21a668 // indirect
+	github.com/go-macaron/inject v0.0.0-20160627170012-d8a0b8677191 // indirect
+	github.com/go-macaron/session v0.0.0-20190131233854-0a0a789bf193 // indirect
+	github.com/go-sql-driver/mysql v1.4.1 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/gosimple/slug v1.5.0 // indirect
+	github.com/grafana/grafana v6.1.3+incompatible
+	github.com/inconshreveable/log15 v0.0.0-20180818164646-67afb5ed74ec // indirect
+	github.com/lib/pq v1.0.0 // indirect
+	github.com/mattn/go-colorable v0.1.1 // indirect
+	github.com/mattn/go-isatty v0.0.7 // indirect
+	github.com/prometheus/client_golang v0.9.2 // indirect
+	github.com/rainycape/unidecode v0.0.0-20150907023854-cb7f23ec59be // indirect
+	github.com/teris-io/shortid v0.0.0-20171029131806-771a37caa5cf // indirect
+	golang.org/x/crypto v0.0.0-20190411191339-88737f569e3a // indirect
+	golang.org/x/oauth2 v0.0.0-20190402181905-9f3314589c9a // indirect
+	gopkg.in/bufio.v1 v1.0.0-20140618132640-567b2bfa514e // indirect
+	gopkg.in/ini.v1 v1.42.0 // indirect
+	gopkg.in/macaron.v1 v1.3.2 // indirect
+	gopkg.in/redis.v2 v2.3.2 // indirect
+)
--- main.go
+++ main.go
@@ -1,93 +1,178 @@
 package main
 
 import (
-    "bytes"
+	"bytes"
 	"encoding/json"
 	"fmt"
-    "io/ioutil"
+	"github.com/grafana/grafana/pkg/components/simplejson"
+	m "github.com/grafana/grafana/pkg/models"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
-    m "github.com/grafana/grafana/pkg/models"
+	"strconv"
+	"strings"
 )
 
 var grafanaUrl string = os.Getenv("GRAFANA_URL")
+var influxdbUrl string = os.Getenv("INFLUXDB_URL")
 
 func main() {
-	fmt.Print("Checking orgs...\n")
 	orgs, err := GetOrgs()
-	if err != nil { 
-		fmt.Print(err)	
+	if err != nil {
+		fmt.Print(err)
 	}
-	fmt.Printf("Found %d\n", len(orgs))
 	for _, org := range orgs {
-		if org.Id == 1 { 
-			continue 
-		}
-		ds, err := GetDataSourceByName(org.Name, "ScreepsPlus-Graphite")
-		if err != nil { 
-			fmt.Print(err)
+		if org.Id == 1 {
 			continue
 		}
-		if ds.Name == "" {
+		if org.Name != strings.ToLower(org.Name) {
+			newName := strings.ToLower(org.Name)
+			log.Printf("Updating name for org %s to %s", org.Name, newName)
+			if err := UpdateOrg(org.Id, m.UpdateOrgCommand{Name: newName}); err != nil {
+				log.Printf("%v", err)
+			}
+			org.Name = newName
+		}
+
+		if ds, _ := GetDataSourceByName(org.Name, "ScreepsPlus-Graphite"); ds == nil {
 			data := m.AddDataSourceCommand{
-				Name: "ScreepsPlus-Graphite",
-				Type: "graphite",
-				Access: "direct",
-				Url: "https://screepspl.us/grafana/carbonapi",
+				Name:            "ScreepsPlus-Graphite",
+				Type:            "graphite",
+				Access:          "direct",
+				Url:             "https://screepspl.us/grafana/carbonapi",
+				WithCredentials: true,
+				JsonData:        &simplejson.Json{},
+			}
+			data.JsonData.Set("graphiteVersion", "1.1.x")
+			if ds, err := AddDataSource(org.Name, data); err != nil {
+				log.Printf("DataSource add failed for %s %v\n", org.Name, err)
+			} else {
+				log.Printf("DataSource %s added for %s\n", ds.Name, org.Name)
+			}
+		}
+
+		if ds, _ := GetDataSourceByName(org.Name, "ScreepsPlus-InfluxDB"); ds == nil {
+			data := m.AddDataSourceCommand{
+				Name:            "ScreepsPlus-InfluxDB",
+				Type:            "influxdb",
+				Access:          "proxy",
+				Url:             influxdbUrl,
 				WithCredentials: true,
 			}
-			ds, _ := AddDataSource(org.Name, data)
-			fmt.Printf("DataSource added %s %v\n", org.Name, ds)
+			if ds, err := AddDataSource(org.Name, data); err != nil {
+				log.Printf("DataSource add failed for %s %v\n", org.Name, err)
+			} else {
+				log.Printf("DataSource %s added for %s\n", ds.Name, org.Name)
+			}
 		}
 	}
 }
 
-func GetOrgs() ([]m.OrgDTO,error) {
-    ret := make([]m.OrgDTO,0)
-    req, _ := http.NewRequest("GET",fmt.Sprintf("%s/api/orgs", grafanaUrl),nil)
-    req.Header.Add("Token-Claim-Sub", "admin")
-    client := &http.Client{}
-    res, err := client.Do(req)
-    if err != nil {
-    	return ret, err
-    }
-    defer res.Body.Close()
-    data, err := ioutil.ReadAll(res.Body)
-    err = json.Unmarshal(data, &ret)
-    return ret, err
+func GetOrgs() ([]m.OrgDTO, error) {
+	ret := make([]m.OrgDTO, 0)
+	req, _ := http.NewRequest("GET", fmt.Sprintf("%s/api/orgs", grafanaUrl), nil)
+	req.Header.Add("Token-Claim-Sub", "admin")
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return ret, err
+	}
+	defer res.Body.Close()
+	data, err := ioutil.ReadAll(res.Body)
+	err = json.Unmarshal(data, &ret)
+	return ret, err
 }
 
-func GetDataSourceByName(user string, name string) (m.DataSource,error) {
-    ret := m.DataSource{}
-    req, _ := http.NewRequest("GET",fmt.Sprintf("%s/api/datasources/name/%s", grafanaUrl, name),nil)
-    req.Header.Add("Token-Claim-Sub", user)
-    client := &http.Client{}
-    res, err := client.Do(req)
-    if err != nil {
-    	return ret, err
-    }
-    defer res.Body.Close()
-    data, err := ioutil.ReadAll(res.Body)
-    err = json.Unmarshal(data, &ret)
-    if err != nil { fmt.Printf("%s %v\n",string(data),err)}
-    return ret, err   
+func GetDataSourceByName(user string, name string) (*m.DataSource, error) {
+	req, _ := http.NewRequest("GET", fmt.Sprintf("%s/api/datasources/name/%s", grafanaUrl, name), nil)
+	req.Header.Add("Token-Claim-Sub", user)
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	dec := json.NewDecoder(res.Body)
+	switch res.StatusCode {
+	case 200:
+		ret := m.DataSource{}
+		err := dec.Decode(&ret)
+		if err != nil {
+			log.Printf("Error Decoding: %v", err)
+			return nil, err
+		}
+		return &ret, nil
+	default:
+		ret := struct{ Message string }{}
+		err := dec.Decode(&ret)
+		if err != nil {
+			log.Printf("Error Decoding: %v", err)
+			return nil, err
+		}
+		log.Printf("Error Getting Datasource %s for %s: %s\n", name, user, ret.Message)
+		return nil, fmt.Errorf("Error: %s", ret.Message)
+	}
 }
 
-func AddDataSource(user string, data m.AddDataSourceCommand) (m.DataSource,error) {
-    jsonStr, _ := json.Marshal(data)
-    req, _ := http.NewRequest("POST",fmt.Sprintf("%s/api/datasources", grafanaUrl),bytes.NewBuffer(jsonStr))
-    req.Header.Add("Token-Claim-Sub", user)
-    ret := m.DataSource{}
-    req.Header.Add("Content-Type", "application/json")
-    req.Header.Add("Content-Length", string(len(jsonStr)))
-    client := &http.Client{}
-    res, err := client.Do(req)
-    if err != nil {
-    	return ret, err
-    }
-    defer res.Body.Close()
-    body, err := ioutil.ReadAll(res.Body)
-    err = json.Unmarshal(body, &ret)
-    if err != nil { fmt.Printf("%s %v\n",string(body),err)}
-    return ret, err
+func AddDataSource(user string, data m.AddDataSourceCommand) (*m.DataSource, error) {
+	jsonStr, _ := json.Marshal(data)
+	req, _ := http.NewRequest("POST", fmt.Sprintf("%s/api/datasources", grafanaUrl), bytes.NewBuffer(jsonStr))
+	req.Header.Add("Token-Claim-Sub", user)
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Content-Length", strconv.Itoa(len(jsonStr)))
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	dec := json.NewDecoder(res.Body)
+	switch res.StatusCode {
+	case 200:
+		ret := m.DataSource{}
+		err := dec.Decode(&ret)
+		if err != nil {
+			log.Printf("Error Decoding: %v", err)
+			return nil, err
+		}
+		return &ret, nil
+	default:
+		ret := struct{ Message string }{}
+		err := dec.Decode(&ret)
+		if err != nil {
+			log.Printf("Error Decoding: %v", err)
+			return nil, err
+		}
+		log.Printf("Error Adding Datasource for %s: %s\n", user, ret.Message)
+		return nil, fmt.Errorf("Error: %s", ret.Message)
+	}
+}
+
+func UpdateOrg(orgId int64, data m.UpdateOrgCommand) error {
+	jsonStr, _ := json.Marshal(data)
+	req, _ := http.NewRequest("PUT", fmt.Sprintf("%s/api/orgs/%d", grafanaUrl, orgId), bytes.NewBuffer(jsonStr))
+	req.Header.Add("Token-Claim-Sub", "admin")
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Content-Length", strconv.Itoa(len(jsonStr)))
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	dec := json.NewDecoder(res.Body)
+	switch res.StatusCode {
+	case 200:
+		return nil
+	default:
+		ret := struct{ Message string }{}
+		err := dec.Decode(&ret)
+		if err != nil {
+			log.Printf("Error Decoding: %v", err)
+			return err
+		}
+		log.Printf("Error Updating org %d: %s\n", orgId, ret.Message)
+		return fmt.Errorf("Error: %s", ret.Message)
+	}
 }
